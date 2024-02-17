12a41d600ab0126c9da36014e2514e4549c36970
zjyhhhhh <1326680198@qq.com>
2024-02-17 12:27:32 -0500

    Update secondary color

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -4,7 +4,7 @@ const dark = "#626262";
 const orange = "#fea415";
 const gray = "#ECECEC";
 const lightBlue = "#f1f4ff";
-const lightOrange = "#F8D0A0"
+const lightOrange = "#FBE7CE"
 const darkGray = "#585757";
 
 export default {
